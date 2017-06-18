a170a835889161e2631ab30d7dbb9d4d254f0727
TheDiamondYT1 <luke.wyatt21@gmail.com>
2017-06-19 00:32:36 +0100

    Some basic curl stuff

---
+++ src/pmdc/utils/CurlUtils.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace pmdc\utils;
+
+class CurlUtils {
+
+    /**
+     * @param string    $url
+     * @param string... $headers
+     */
+    public static function curlGet(string $url, ...$headers) {
+        $curl = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_HEADER, 1);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        
+        $response = curl_exec($curl);
+        curl_close($curl);
+        
+        return $response;
+    }
+}
